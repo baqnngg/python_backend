d4c57bd7434c19d4afcf0266d9bad575266f0b5e
baqnngg <il8696546@gmail.com>
2024-05-24 14:50:00 +0900

    msg

--- rewind/app.py
+++ rewind/app.py
@@ -19,5 +19,26 @@ def check_users():
     return app.users
 
 
+@app.route("/post", methods=['POST'])
+def post():
+    payload = request.json
+    userID = int(payload['id'])
+    msg = payload['msg']
+
+    if userID not in app.users:
+        return "사용자가 존재하지 않습니다.", 400
+    if len(msg) > 300:
+        return "300자를 초과했습니다.", 400
+
+    app.posts.append({
+        'user_id': userID,
+        'tweet': msg
+    })
+    return '성공', 200
+
+@app.route("/check-tweet", methods=['GET'])
+def check_tweet():
+    return app.posts
+
 if __name__ =='__main__':
     app.run(debug=True)
